dc86d19bc26c28509124e87188c0125d723926f7
Anakin <al5916@princeton.edu>
2024-12-04 19:57:08 +0000

    Adding more comments and minor typo fixes

--- SentenceTokenizer.java
+++ SentenceTokenizer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class SentenceTokenizer {
@@ -18,6 +19,26 @@ public class SentenceTokenizer {
         for (int i = 0; i < tokens.length; i++) {
             tokens[i] = tokenizer.nextToken();
         }
+
+    }
+
+    // Constructor for the sentence tokenizer with stop words
+    public SentenceTokenizer(String sentence, StopWords stopWords) {
+
+        // Initialize a StringTokenizer class to handle basic tokenization (adding hypen and apostrophes as delimeters for Igbo)
+        StringTokenizer tokenizer = new StringTokenizer(sentence, " -'!?");
+
+        // Initialize the array for the tokens length amount of tokens
+        ArrayList<String> tokenList = new ArrayList<>();
+
+        // Add each token to the array
+        for (int i = 0; i < tokenizer.countTokens(); i++) {
+            tokenList.add(tokenizer.nextToken());
+        }
+
+        // Convert the tokens to an array
+        tokens = (String[])(tokenList.toArray());
+
     }
 
     // Return the tokens array once requested
--- StopWords.java
+++ StopWords.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 
 public class StopWords {
 
+    // Create a HashMap to be able to quickly access a stop word's existence
     private HashMap<String, Boolean> stopWords = new HashMap<>();
 
+    // Return the HashMap of the stop words
     public HashMap<String, Boolean> getStopWords() {
         return stopWords;
     }
-
+    
+    // Set the stop words to a new HashMap
     public void setStopWords(HashMap<String, Boolean> stopWords) {
         this.stopWords = stopWords;
     }
 
+    // Construct the list of stop words from a list file
     public StopWords(File list) throws FileNotFoundException {
         stopWords = new HashMap<>();
 
+        // Go through each word in the stop word list and add them to the HashMap
         try (Scanner scan = new Scanner(list)) {
             while (scan.hasNext()) {
                 stopWords.put(scan.next(), true);
