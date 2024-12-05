d25cc2975a091e4dac56a3dfd56331a1a344babb
Anakin <al5916@princeton.edu>
2024-12-05 00:34:53 +0000

    Minor fixes pt 2

--- ParagraphSentiment.java
+++ ParagraphSentiment.java
@@ -6,6 +6,9 @@ import java.util.Set;
 
 public class ParagraphSentiment {
 
+    double chancePositive;
+    double chanceNegative;
+    double chanceNeutral;
 
     public ParagraphSentiment(File paragraph, String splitParagraphFile, File stopWords, FrequencyCounter freqCount, WeightedWords weights) throws IOException {
 
@@ -15,48 +18,60 @@ public class ParagraphSentiment {
 
         File splitParagraph = new File(splitParagraphFile);
 
-        Scanner scan2 = new Scanner(splitParagraph);
-    
-        HashMap<String,Integer> posFreq = freqCount.getPositiveFrequencies();
-        HashMap<String,Integer> negFreq = freqCount.getNegativeFrequencies();
-        HashMap<String,Integer> neuFreq = freqCount.getNeutralFrequencies();
-        Set<String> uniqueFreq = freqCount.getWords();
+        try (Scanner scan2 = new Scanner(splitParagraph)) {
+            HashMap<String,Integer> posFreq = freqCount.getPositiveFrequencies();
+            HashMap<String,Integer> negFreq = freqCount.getNegativeFrequencies();
+            HashMap<String,Integer> neuFreq = freqCount.getNeutralFrequencies();
+            Set<String> uniqueFreq = freqCount.getWords();
 
-        double chancePositive = 1;
-        double chanceNegative = 1;
-        double chanceNeutral = 1;
+            chancePositive = 1;
+            chanceNegative = 1;
+            chanceNeutral = 1;
 
-        while (scan2.hasNextLine()) {
-            SentenceTokenizer currentTokens = new SentenceTokenizer(scan2.nextLine(), igboStop);
+            while (scan2.hasNextLine()) {
+                SentenceTokenizer currentTokens = new SentenceTokenizer(scan2.nextLine(), igboStop);
 
-            for (String token : currentTokens.getTokens()) {
+                for (String token : currentTokens.getTokens()) {
 
-                int posFrequency = 0;
-                int negFrequency = 0;
-                int neuFrequency = 0;
+                    int posFrequency = 0;
+                    int negFrequency = 0;
+                    int neuFrequency = 0;
 
-                if (posFreq.containsKey(token)) {
-                    posFrequency += posFreq.get(token);
+                    if (posFreq.containsKey(token)) {
+                        posFrequency += posFreq.get(token);
+                    }
+
+                    if (negFreq.containsKey(token)) {
+                        negFrequency += negFreq.get(token);
+                    }
+
+                    if (neuFreq.containsKey(token)) {
+                        neuFrequency += neuFreq.get(token);
+                    }
+
+                    chancePositive *= (posFrequency + 1) / (posFreq.size() + uniqueFreq.size());
+
+                    chanceNegative *= (negFrequency + 1) / (negFreq.size() + uniqueFreq.size());
+
+                    chanceNeutral *= (neuFrequency + 1) / (neuFreq.size() + uniqueFreq.size());
+
                 }
-
-                if (negFreq.containsKey(token)) {
-                    negFrequency += negFreq.get(token);
-                }
-
-                if (neuFreq.containsKey(token)) {
-                    neuFrequency += neuFreq.get(token);
-                }
-
-                chancePositive *= (posFrequency + 1) / (posFreq.size() + uniqueFreq.size());
-
-                chanceNegative *= (negFrequency + 1) / (negFreq.size() + uniqueFreq.size());
-
-                chanceNeutral *= (neuFrequency + 1) / (neuFreq.size() + uniqueFreq.size());
 
             }
-
         }
 
+    }
+
+    public double positiveChance() {
+        return chancePositive;
+    }
+
+    public double negativeChance() {
+        return chanceNegative;
+    }
+
+    public double neutralChance() {
+        return chanceNeutral;
     }
 
     public static void main(String[] args) {
